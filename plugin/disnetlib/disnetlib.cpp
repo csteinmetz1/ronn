cfdcd44e20e0b6d7330b77b4ec48b55a4d995f5f
csteinmetz1 <csteinmetz1@gmail.com>
2020-04-26 22:20:27 +0200

    adding multiple activation functions, and also adding dilations

--- plugin/disnetlib/disnetlib.cpp
+++ plugin/disnetlib/disnetlib.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 #include<string>
-#include <cstdint>
+#include<cstring>
+#include<cstdint>
 #include<torch/torch.h>
 
 struct Model : torch::nn::Module {
-    Model(int nInputs, int nOutputs, int nLayers, int nChannels, int kernelWidth, bool bias) {
+    Model(int nInputs, 
+          int nOutputs, 
+          int nLayers, 
+          int nChannels, 
+          int kernelWidth, 
+          bool bias, 
+          std::string act,
+          float* dilations) {
 
         for (int i = 0; i < nLayers; i++) {
             if (i == 0) {
@@ -22,8 +30,11 @@ struct Model : torch::nn::Module {
             conv.push_back(torch::nn::Conv1d(
                 torch::nn::Conv1dOptions(inChannels,outChannels,kernelWidth)
                 .stride(1)
+                .dilation(dilations[i])
                 .bias(bias)));
         }
+
+        activation = act; // set the activation function
 
         // now register each convolutional layer
         for (auto i = 0; i < conv.size(); i++) {
@@ -36,11 +47,32 @@ struct Model : torch::nn::Module {
                         .negative_slope(0.2));
     }
     // the forward operation
-    torch::Tensor forward(torch::Tensor x){
+    torch::Tensor forward(torch::Tensor x) {
         // we iterate over the convolutions
         for (auto i = 0; i < conv.size(); i++) {
             if (i + 1 < conv.size()) {
-                x = torch::relu(conv[i](x));
+                if (activation.compare("ReLU") == 0)
+                    x = torch::relu(conv[i](x));
+                else if (activation.compare("LeakyReLU") == 0)
+                    x = leakyrelu(conv[i](x));
+                else if (activation.compare("Tanh") == 0)
+                    x = torch::tanh(conv[i](x));
+                else if (activation.compare("Sigmoid") == 0)
+                    x = torch::tanh(conv[i](x));
+                else if (activation.compare("ELU") == 0)
+                    x = torch::elu(conv[i](x));
+                else if (activation.compare("SELU") == 0)
+                    x = torch::selu(conv[i](x));
+                else if (activation.compare("GELU") == 0)
+                    x = torch::gelu(conv[i](x));
+                else if (activation.compare("RReLU") == 0)
+                    x = torch::rrelu(conv[i](x));
+                else if (activation.compare("Softplus") == 0)
+                    x = torch::softplus(conv[i](x));
+                else if (activation.compare("Softshrink") == 0)
+                    x = torch::softshrink(conv[i](x));
+                else
+                    x = conv[i](x);
             }
             else {
                 x = conv[i](x);
@@ -48,14 +80,30 @@ struct Model : torch::nn::Module {
         }
         return x;
     }
+
+    void init(std::string initType){
+        for (auto i = 0; i < conv.size(); i++) {
+            if (initType.compare("normal"))
+                torch::nn::init::normal_(conv[i]->weight);
+            else if (initType.compare("uniform"))
+                torch::nn::init::normal_(conv[i]->weight, -0.25, 0.25);
+            else if (initType.compare("xavier_normal"))
+                torch::nn::init::xavier_normal_(conv[i]->weight);
+            else if (initType.compare("xavier_uniform"))
+                torch::nn::init::xavier_uniform_(conv[i]->weight);
+            else if (initType.compare("kaiming_normal"))
+                torch::nn::init::kaiming_normal_(conv[i]->weight);
+            else if (initType.compare("kamming_uniform"))
+                torch::nn::init::kaiming_uniform_(conv[i]->weight);            
+        }
+    }
+
     int inChannels, outChannels;
     std::vector<torch::nn::Conv1d> conv;
 
     // activation functions
-    torch::nn::ReLU relu;
-    torch::nn::Tanh tanh;             
+    std::string activation;         
     torch::nn::LeakyReLU leakyrelu;
-    torch::nn::
 };
 
 int main(){
@@ -70,9 +118,18 @@ int main(){
     int nChannels   = 8;
     int kernelWidth = 3;
     bool bias       = false;
+    std::string act = "Softshrink";
+    float dilations [6] = {1,2,4,8,16,32};
 
     // build the model
-    Model model(nInputs, nOutputs, nLayers, nChannels, kernelWidth, bias);
+    Model model(nInputs, 
+                nOutputs, 
+                nLayers, 
+                nChannels, 
+                kernelWidth, 
+                bias, 
+                act,
+                dilations);
     
     // pass through a random tensor
     auto in = torch::rand({1,1,44100});
@@ -81,5 +138,7 @@ int main(){
     for (auto i = 0; i < out.dim(); i++){
         std::cout << out.size(i) << std::endl;
     }
+
+    model.init("xavier_normal");
 
 }
