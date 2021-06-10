9ab4d7d58cb67203c344fec9371c2561630784ba
csteinmetz1 <csteinmetz1@gmail.com>
2021-06-10 09:17:46 -0400

    incorrect link to juce

--- plugin/juce/ronn/build.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build
-mkdir build
-cd build
-# Update the absolute path to libtorch (ensure you leave .. at the end)
-cmake .. -G Xcode -DCMAKE_PREFIX_PATH=/absolute/path/to/libtorch ..
-cmake --build .
